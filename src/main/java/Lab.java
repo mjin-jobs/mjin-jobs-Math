d5dbc53128d51a34e041686d04815b6a3ee0e787
mjin-jobs <mjin.jobs@gmail.com>
2023-02-09 01:53:33 +0000

    01/28 - 2

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,7 @@ public class Lab {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return 0;
+        double ans = Math.pow(a, b);
+        return ans;
     }
 }
